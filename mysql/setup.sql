e1dc96a7b778919c904c54d3480eacefad30f967
israel de abreu gonçalves <israel.goncalves@mc1.com.br>
2022-03-31 12:03:25 -0300

    correções

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,10 +1,7 @@
 use nodedb
 
 create table people(
-    id int auto_increment primary key,
-    name varchar(250)
+    ID int AUTO_INCREMENT PRIMARY KEY,
+    NAME varchar(50) NOT NULL,
+    CREATED datetime
 );
-
-insert into people values (1,'Carlos');
-insert into people values  (2,'Pedro');
-insert into people values (3,'Israel');
--- node/index.js
+++ node/index.js
@@ -2,53 +2,64 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-/*
+
 const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    database:'nodedb'
+    database: 'nodedb'
 };
 const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Israel')`
-connection.query(sql)
-connection.end()
-*/
+try {
+    const connection = mysql.createConnection(config)
 
-app.get('/', (req,res) => {
-    
-    var mysql = require('mysql');
+    const sql = `INSERT INTO people(name,created) values('Israel',now())`
+    connection.query(sql)
+    connection.end()
+} catch (error) {
 
-    var con = mysql
-    .createConnection({
-        host: 'db',
-        user: 'root',
-        password: 'root',
-        database:'nodedb'
-    });
-    
-    con.connect(function(err) {
-      if (err) throw err;
-      con.query("SELECT * FROM people;", function (err, result, fields) {
-        if (err) throw err;
+}
 
-        var html = "<table><tr><td>ID</td><td>NOME</td><tr>";
+app.get('/', (req, res) => {
 
-        Object.keys(result).forEach(function(key) {
-            var row = result[key];
-            html = html + "<tr><td>" + row.id + "</td><td>" + row.name + "</td><tr>";
-          });
+    try {
 
-          html = html + "</table>";
+        var mysql = require('mysql');
 
-          res.send(html);
+        var con = mysql
+            .createConnection({
+                host: 'db',
+                user: 'root',
+                password: 'root',
+                database: 'nodedb'
+            });
 
-      });
-    });
+        con.connect(function (err) {
+            if (err) throw err;
+            con.query("SELECT * FROM people;", function (err, result, fields) {
+                if (err) throw err;
+
+                var html = "<h1>Full Cycle Rocks!</h1>";
+                html = html + "<br><br>";
+                html = html + "<table><tr><td>ID</td><td>NOME</td><td>DATA</td><tr>";
+
+                Object.keys(result).forEach(function (key) {
+                    var row = result[key];
+                    html = html + "<tr><td>" + row.ID + "</td><td>" + row.NAME + "</td><td>" + row.CREATED + "</td><tr>";
+                });
+
+                html = html + "</table>";
+
+                res.send(html);
+
+            });
+        });
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log('Rodando na porta ' + port)
 })
